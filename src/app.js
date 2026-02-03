e0be0989ce756690ec49a3b8fc6a9a03f18a2386
d3kv <76914071+d3kv@users.noreply.github.com>
2026-02-03 10:06:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function generarExcusa() {
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+let WhoPicker = Math.floor(Math.random()*who.length);
+let ActionPicker = Math.floor(Math.random()*action.length);
+let WhatPicker = Math.floor(Math.random()*what.length);
+let Whenpicker = Math.floor(Math.random()*when.length);
+
+return who[WhoPicker] + " " + action[ActionPicker]+ " " + what[WhatPicker] + " "+ when[Whenpicker];
+}
+document.getElementById("excuse").innerHTML = generarExcusa ();
+}
